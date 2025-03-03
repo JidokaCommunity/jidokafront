77e6ad8fc18e93af6d8615ce3cb04dce1952d3a5
Nailyn DFl <nailyn.diazf@gmail.com>
2025-03-02 20:55:36 -0500

    Add who we are page

---
+++ app/components/WhoWeAre.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const WhoWeAre = () => {
+  return <main className="p-8">
+    <div className="mb-4">
+      <h1 className="text-center text-6xl font-jidoka-newsreader">Who we are</h1>
+    </div>
+    <p className="max-w-4xl mx-auto text-justify">We are a nonprofit community dedicated to empowering aspiring
+     developers through hands-on, project-based learning. We believe that real-world projects are the best way to
+     practice and master software development skills, regardless of your background or prior education.</p>
+    <p className="max-w-4xl mx-auto text-justify">By coming together, sharing knowledge, and collaborating on exciting
+     projects, we create an inclusive environment where everyone can grow and transform their lives through technology.
+     Whether you're just starting out or looking to sharpen your skills, our community is here to support you on your
+      journey to becoming a software developer. </p>
+  </main>
+}
+
+export default WhoWeAre
---
+++ app/who-we-are/page.tsx
@@ -0,0 +1,5 @@
+import WhoWeAre from '../components/WhoWeAre';
+
+export default function WhoWeArePage() {
+  return <WhoWeAre />;
+}
