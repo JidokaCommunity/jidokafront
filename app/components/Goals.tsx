a6811463d6c94f7b1e1eee70a6541f380eea23df
Stiven Valencia <s.valenciar0@gmail.com>
2025-11-05 19:32:29 -0500

    feat: add Goals component and page with routing; include tests for rendering and styles (merge dev)

---
+++ app/components/Goals.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+import Link from "next/link";
+
+const Goals: FC = () => {
+    return (
+        <main>
+            <div className="flex justify-center mt-28">
+                <h1 className="w-[1020px] text-center text-6xl font-jidoka-newsreader">
+                    Goals
+                </h1>
+            </div>
+        </main>
+    )
+}
+
+export default Goals;
