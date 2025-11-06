ca5911b428684390ebbb02c33a8d6cf271a7243f
Stiven Valencia <s.valenciar0@gmail.com>
2025-11-05 19:33:09 -0500

    refactor: update Goals and Vision components for consistency in styling

--- app/components/Goals.tsx
+++ app/components/Goals.tsx
@@ -1,11 +1,10 @@
 import React, { FC } from "react";
-import Link from "next/link";
 
 const Goals: FC = () => {
     return (
         <main>
-            <div className="flex justify-center mt-28">
-                <h1 className="w-[1020px] text-center text-6xl font-jidoka-newsreader">
+            <div className="min-h-screen bg-white flex justify-center">
+                <h1 className="text-3xl sm:text-3xl md:text-5xl font text-gray-900 font-jidoka-newsreader p-6">
                     Goals
                 </h1>
             </div>
